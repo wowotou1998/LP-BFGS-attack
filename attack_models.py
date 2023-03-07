fix L0 computation, add JSMA comparison

attack_models.py
@@ -78,8 +78,8 @@ def random_target(y_original, num_classes):
     Return:
         target_labels(list): random target labels
     """
-    y_adv = torch.zeros_like(y_original, device=y_original.device, dtype=int)
-    batch, _ = y_original.shape
+    y_adv = torch.zeros_like(y_original, device=y_original.device, dtype=torch.long)
+    batch = y_original.numel()
     # sample(list, k) 返回一个长度为k新列表，新列表存放list所产生k个随机唯一的元素
     for i in range(batch):
         # 随机标签列表
@@ -87,7 +87,7 @@ def random_target(y_original, num_classes):
         label_set = list(range(num_classes))
         label_set_2 = label_set[0:y_o_i] + label_set[y_o_i + 1:num_classes]
         y_adv_i = random.sample(label_set_2, 1)
-        y_adv[i] = y_adv_i
+        y_adv[i] = y_adv_i[0]
 
     return y_adv
 
@@ -219,7 +219,6 @@ def attack_by_k_pixels(attack_name, model, images, labels, eps, trade_off_c, A, 
 
     if attack_name == 'JSMA':
         start = time.perf_counter()
-        # if theta<0, it will be an untarget attack
         atk = JSMA(model, num_classes=10,
                    theta=1., gamma=1.0 * KP.numel() / RP.numel(),
                    clip_min=0.0, clip_max=1.0,
@@ -348,7 +347,7 @@ def attack_by_k_pixels(attack_name, model, images, labels, eps, trade_off_c, A, 
     raise Exception('Unknown attack method')
 
 
-def attack_one_model(model, test_loader, test_loader_size, attack_set, N, eps, trade_off_c, pixel_k):
+def attack_one_model(model, test_loader, test_loader_size, attack_set, N, eps, trade_off_c, pixel_k, SHOW=False):
     cifar_label = {0: "airplane", 1: "car", 2: "bird", 3: "cat", 4: "deer",
                    5: "dog", 6: "frog", 7: "horse", 8: "ship", 9: "truck"}
     device = torch.device("cuda:%d" % (0) if torch.cuda.is_available() else "cpu")
@@ -421,8 +420,9 @@ def attack_one_model(model, test_loader, test_loader_size, attack_set, N, eps, t
         acc_num_before_attack += predict_answer.sum().item()
         # 统计神经网络分类正确的样本的个数总和
         # valid_attack_num += labels.shape[0]
-        plot_images = images.detach().clone()
-        plot_titles = ['original: ' + str(labels[0].item())]
+        if SHOW:
+            plot_images = images.detach().clone()
+            plot_titles = ['original: ' + str(labels[0].item())]
         for idx, attack_i in enumerate(attack_set):
             images_under_attack, time_i = attack_by_k_pixels(attack_i, model, images, labels, eps,
                                                              trade_off_c, A, KP, RP)
@@ -432,7 +432,8 @@ def attack_one_model(model, test_loader, test_loader_size, attack_set, N, eps, t
 
             noise = images_under_attack.detach().clone().view(images.shape[0], -1) - \
                     images.detach().clone().view(images.shape[0], -1)
-            noise = torch.index_select(noise, 1, pixel_idx)
+            if attack_i not in ['SparseFool', 'JSMA']:
+                noise = torch.index_select(noise, 1, pixel_idx)
             noise_norm0 = torch.linalg.norm(noise, ord=0, dim=1)
             noise_norm1 = torch.linalg.norm(noise, ord=1, dim=1)
             noise_norm2 = torch.linalg.norm(noise, ord=2, dim=1)
@@ -461,9 +462,9 @@ def attack_one_model(model, test_loader, test_loader_size, attack_set, N, eps, t
             noise_norm1_total[idx] += valid_noise_norm1.sum().item()
             noise_norm2_total[idx] += valid_noise_norm2.sum().item()
             noise_norm_inf_total[idx] += valid_noise_norm_inf.sum().item()
-
-            # plot_images = torch.cat([plot_images, images_under_attack.clone().detach()], dim=0)
-            # plot_titles += [attack_i + ': ' + str(predict[0].item())]
+            if SHOW:
+                plot_images = torch.cat([plot_images, images_under_attack.clone().detach()], dim=0)
+                plot_titles += [attack_i + ': ' + str(predict[0].item())]
             if acc_num_before_attack == 1:
                 pass
 
@@ -524,9 +525,9 @@ def attack_one_model(model, test_loader, test_loader_size, attack_set, N, eps, t
                 # fig.savefig('pixel_selecor2.pdf')
                 # -------- plot attribution score --------
 
-            # break
-        # if acc_num_before_attack == 1:
-        # show_images(plot_images, plot_titles)
+            # brea
+        if SHOW:
+            show_images(plot_images, plot_titles)
         if epoch_num >= N:
             break
     print('attack_success_num', attack_success_num)
@@ -564,6 +565,7 @@ def attack_many_model(job_name, dataset, model_name_set, attack_N, attack_set, b
     res_data = [['dataset', 'mode_name', 'attack_method', 'attack_num', 'constant c', 'eps_i', 'pixel_k',
                  'attack_success', 'confidence', 'noise_norm0', 'noise_norm1', 'noise_norm2',
                  'noise_norm_inf', 'time(ms)']]
+
     for set_i, dataset_i in enumerate(dataset):
         test_loader, test_dataset_size = load_dataset(dataset_i, batch_size, is_shuffle=True)
         for mode_name in model_name_set[set_i]:
@@ -591,13 +593,14 @@ def attack_many_model(job_name, dataset, model_name_set, attack_N, attack_set, b
                         res_data.append(
                             [dataset_i, mode_name, attack_set[i], attack_N, trade_off_c, eps_i, pixel_k,
                              success_rate[i], confidence[i], norm0[i], norm1[i], norm2[i], norm_inf[i], time[i]])
-    current_time = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
-    with open('./Checkpoint/%s_%s.pkl' % (job_name, current_time), 'wb') as f:
-        pickle.dump(res_data, f)
-    import pandas as pd
-    test = pd.DataFrame(columns=res_data[0], data=res_data[1:])
-    test.to_csv('./Checkpoint/%s_%s.csv' % (job_name, datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")))
-    print(test)
+
+        current_time = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+        with open('./Checkpoint/%s_%s.pkl' % (job_name, current_time), 'wb') as f:
+            pickle.dump(res_data, f)
+        import pandas as pd
+        csv = pd.DataFrame(columns=res_data[0], data=res_data[1:])
+        csv.to_csv('./Checkpoint/%s_%s.csv' % (job_name, datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")))
+        print(csv)
 
     # with open('%s.pkl' % ('pkl'), 'rb') as f:
     #     basic_info = pickle.load(f)
@@ -637,7 +640,7 @@ if __name__ == '__main__':
     torch.cuda.manual_seed(123)
 
     batch_size = 1
-    attack_N = 1000
+    attack_N = 1
     # pixel_k_set = [20]
     # pixel_k_set = [5, 10, 15]
     # pixel_k_set = [10]
@@ -651,28 +654,28 @@ if __name__ == '__main__':
 
     # job_name = 'cifar_%d_diff_loss_20pixel_1e3' % attack_N
 
-    job_name = 'mni_cifar_iter200_%d_100acc_20pixel_1e3' % attack_N
-
+    # job_name = 'mni_cifar_iter200_%d_100acc_20pixel_1e3' % attack_N
+    job_name = 'jsma_iter200_%d_100acc_20pixel_1e3' % attack_N
     attack_many_model(job_name,
-                      ['MNIST', ],
-                      # ['ImageNet'],
-                      [mnist_model_name_set, ],
-                      # [imagenet_model_name_set],
-                      attack_N=20,
+                      ['MNIST', 'CIFAR10', 'ImageNet'],
+
+                      [mnist_model_name_set, cifar10_model_name_set, imagenet_model_name_set],
+
+                      attack_N=1000,
                       attack_set=[
                           # 'limited_BFGS_CW',
-                          'limited_BFGS_CE',
+                          # 'limited_BFGS_CE',
                           # 'limited_BFGS_CW_LOG',
                           # 'limited_FGSM',
                           # 'limited_CW',
                           # 'SparseFool',
-                          # 'JSMA'
+                          'JSMA'
                       ],
                       batch_size=1,
                       eps_set=[1.0],
                       trade_off_c=1e3,
-                      pixel_k_set=[40]
-                      # pixel_k_set=[20, 40, 60, 80, 100]
+                      # pixel_k_set=[40]
+                      pixel_k_set=[20, 40, 60, 80, 100]
                       )
 
     # job_name = 'imagenet_%d_100acc_20pixel_1e3' % attack_N

plot_loss.py
@@ -124,8 +124,8 @@ def plot_loss_3d(sample_original, label, model):
 
     # --------------------------准备基向量,确定坐标轴的大致形状---------------------------------
 
-    x_i = np.linspace(-1, 1, 50)
-    y_i = np.linspace(-1, 1, 50)
+    x_i = np.linspace(0, 1, 50)
+    y_i = np.linspace(0, 1, 50)
     ii, jj = np.meshgrid(x_i, y_i)  # 获得网格坐标矩阵
 
     sample_a = torch.zeros_like(sample_original, dtype=torch.float)
@@ -200,7 +200,7 @@ def select_a_sample_to_plot(dataset, mode_name):
 
 
 if __name__ == '__main__':
-    mpl.rcParams['font.sans-serif'] = ['Times New Roman']
+    mpl.rcParams['font.sans-serif'] = ['Arial']
     # 解决保存图像是负号'-'显示为方块的问题
     mpl.rcParams['axes.unicode_minus'] = False
     mpl.rcParams['savefig.dpi'] = 200  # 保存图片分辨率
@@ -214,15 +214,56 @@ if __name__ == '__main__':
     # mpl.rcParams['figure.constrained_layout.use'] = True
 
     model_name_set = ['VGG16', 'VGG19', 'ResNet50', 'ResNet101', 'DenseNet121']
+
+
     # select_a_sample_to_plot('MNIST',
     #                         'ResNet18_ImageNet',
     #                         Epsilon=5 / 255,
     #                         Iterations=10,
     #                         Momentum=1.0)
 
-    select_a_sample_to_plot('MNIST',
-                            'FC_256_128',
-                            )
+    # select_a_sample_to_plot('MNIST',
+    #                         'FC_256_128')
+    def example_plot(ax, fontsize=12, hide_labels=False):
+        pc = ax.pcolormesh(np.random.randn(30, 30), vmin=-2.5, vmax=2.5)
+        if not hide_labels:
+            ax.set_xlabel('x-label', fontsize=fontsize)
+            ax.set_ylabel('y-label', fontsize=fontsize)
+            ax.set_title('Title', fontsize=fontsize)
+        return pc
+
+
+    fig = plt.figure(layout='constrained', figsize=(10, 4))
+    subfigs = fig.subfigures(1, 2, wspace=0.07, width_ratios=[2, 1])
+
+    # -------------------sub-figure-1
+    # sharex 和 sharey 表示坐标轴的属性是否相同，可选的参数：True，False，row，col，默认值均为False，表示画布中的四个ax是相互独立的；
+    # True 表示所有子图的x轴（或者y轴）标签是相同的，
+    # row 表示每一行之间的子图的x轴（或者y轴）标签是相同的（不同行的子图的轴标签可以不同），
+    # col表示每一列之间的子图的x轴（或者y轴）标签是相同的（不同列的子图的轴标签可以不同）
+    axsLeft = subfigs[0].subplots(1, 2, sharey=True)
+    subfigs[0].set_facecolor('0.75')
+    for ax in axsLeft:
+        pc = example_plot(ax)
+    subfigs[0].suptitle('Left plots', fontsize='x-large')
+    subfigs[0].colorbar(pc, shrink=0.6, ax=axsLeft, location='bottom')
+
+    # ----------------sub-figure-2
+    axsRight = subfigs[1].subplots(3, 1, sharex=True)
+    for nn, ax in enumerate(axsRight):
+        pc = example_plot(ax, hide_labels=True)
+        if nn == 2:
+            ax.set_xlabel('xlabel')
+        if nn == 1:
+            ax.set_ylabel('ylabel')
+
+    subfigs[1].set_facecolor('0.85')
+    subfigs[1].colorbar(pc, shrink=0.6, ax=axsRight)
+    subfigs[1].suptitle('Right plots', fontsize='x-large')
+
+    fig.suptitle('Figure suptitle', fontsize='xx-large')
+
+    plt.show()
 
     print()
     print("----ALL WORK HAVE BEEN DONE!!!----")